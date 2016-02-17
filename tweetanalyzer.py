a18a64df3de08d5479f6d19b7f4476c7d5755a66
mr-justdoit <jasonss772@gmail.com>
2016-02-18 02:06:58 +0900

    update

--- tweetanalyzer.py
+++ tweetanalyzer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 from __future__ import absolute_import, print_function
-
-import tweepy,json,sys
+from janome.tokenizer import Tokenizer
+from itertools import groupby
+import tweepy, json, sys, re
 
 with open('data.json') as data_file:
-        data = json.load(data_file)
+    data = json.load(data_file)
 
 query = sys.argv[1]
         
@@ -14,9 +15,43 @@ consumer_secret=data["consumer"]["secret"]
 access_token=data["token"]["key"]
 access_token_secret=data["token"]["secret"]
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.secure = True
-auth.set_access_token(access_token, access_token_secret)
+def twitterAuth(consumer_key, consumer_secret, access_token, access_token_secret):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.secure = True
+    auth.set_access_token(access_token, access_token_secret)
+    return auth
 
-api = tweepy.API(auth)
-print(api.search(q=query))
+def getAPI(consumer_key, consumer_secret, access_token, access_token_secret):
+    auth = twitterAuth(consumer_key, consumer_secret, access_token, access_token_secret)
+    return tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+def getTextsOnTweets(api):
+    results = api.search(q=query,rpp=100)
+    textdata = ""
+
+    for i in range(0, len(results["statuses"])):
+        textdata += results["statuses"][i]["text"]
+
+    return textdata
+
+def textdataToArray(textdata):
+    textdata = re.sub(r'https?:\/\/.*[\r\n]*', '', textdata, flags=re.MULTILINE)
+    textdata = sorted(textdata.split(' '))
+    return textdata
+
+def countWords(words):
+    keys = [key for key, group in groupby(words)]
+    values = [len(list(group)) for key, group in groupby(words)]
+    return dict(zip(keys, values))
+    
+api = getAPI(consumer_key, consumer_secret, access_token, access_token_secret)
+textdata = getTextsOnTweets(api)
+words = textdataToArray(textdata)
+dictionary = countWords(words)
+
+print(dictionary)
+
+
+#t = Tokenizer()
+#tokens = t.tokenize(textdata)
+
