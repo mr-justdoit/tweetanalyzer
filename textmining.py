8884bfc7d9eedcbec6ccfeef1024da8e80c5b285
mr-justdoit <jasonss772@gmail.com>
2016-02-26 13:41:32 +0900

    update

--- textmining.py
+++ textmining.py
@@ -3,35 +3,46 @@ from __future__ import absolute_import, print_function
 from janome.tokenizer import Tokenizer
 from itertools import groupby
 from nltk.tokenize import TweetTokenizer
-import sys, re, yaml, nltk, pyaml
+import sys
+import re
+import nltk
+import pyaml
+
 
 def filter(text):
     text = re.sub(r'[a-zA-Z0-9¥"¥.¥,¥@]+', '', text)
     text = re.sub(r'[!"“#$%&()\*\+\-\.,\/:;<=>?@\[\\\]^_`{|}~]', '', text)
     text = re.sub(r'[\n|\r|\t]', '', text)
-    
-    jp_chartype_tokenizer = nltk.RegexpTokenizer(u'([ぁ-んー]+|[ァ-ンー]+|[\u4e00-\u9FFF]+|[ぁ-んァ-ンー\u4e00-\u9FFF]+)')
+
+    jp_chartype_tokenizer = nltk.RegexpTokenizer(
+        u'([ぁ-んー]+|[ァ-ンー]+|[\u4e00-\u9FFF]+|[ぁ-んァ-ンー\u4e00-\u9FFF]+)')
     text = "".join(jp_chartype_tokenizer.tokenize(text))
     return text
+
 
 def text_to_array(textdata):
     t = TweetTokenizer()
     textdata = sorted(t.tokenize(textdata))
     return textdata
 
+
 def count_words(words):
     keys = [key for key, group in groupby(words)]
     values = [len(list(group)) for key, group in groupby(words)]
     return dict(zip(keys, values))
 
+
 def output_ja_text(data, wordtypes):
     textdata = filter(data)
     t = Tokenizer()
     tokens = t.tokenize(textdata)
-    words = sorted([token.surface for token in tokens if token.part_of_speech.split(',')[0] in wordtypes])
+    words = sorted([token.surface
+                    for token in tokens
+                    if token.part_of_speech.split(',')[0] in wordtypes])
     dictionary = count_words(words)
     return pyaml.dump(dictionary, sys.stdout, vspacing=[0, 1])
-    
+
+
 def output_textdata(data):
     textdata = data
     words = text_to_array(textdata)
--- tweetanalyzer.py
+++ tweetanalyzer.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python
 # coding: utf-8
 from __future__ import absolute_import, print_function
-import tweepy, json, sys, re, yaml, getopt, pprint, nltk, codecs, pyaml
-from textmining import *
+import tweepy
+import json
+import sys
+import getopt
+import pyaml
+import textmining
 
 with open('data.json') as data_file:
     data = json.load(data_file)
 
-def twitter_auth(consumer_key, consumer_secret, access_token, access_token_secret):
+
+def twitter_auth(consumer_key, consumer_secret, access_token,
+                 access_token_secret):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.secure = True
     auth.set_access_token(access_token, access_token_secret)
     return auth
 
+
 def load_api(consumer_key, consumer_secret, access_token, access_token_secret):
-    auth = twitter_auth(consumer_key, consumer_secret, access_token, access_token_secret)
-    return tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+    return tweepy.API(
+        twitter_auth(consumer_key, consumer_secret, access_token,
+                     access_token_secret),
+        parser=tweepy.parsers.JSONParser())
+
 
 def text_on_tweet(api, query, count, page, lang):
     results = api.search(q=query, count=count, lang=lang)
@@ -23,77 +33,89 @@ def text_on_tweet(api, query, count, page, lang):
     max_id = 0
     for j in range(0, page):
         for i in range(0, len(results["statuses"])):
-            textdata += results["statuses"][i]["text"]
+            d = results["statuses"][i]
+            textdata += d["text"]
             textdata += ' '
-            max_id = results["statuses"][i]["id"]
-        results = api.search(q=query, count=count, lang=lang, max_id=max_id-1)
+            max_id = d["id"]
+        results = api.search(q=query,
+                             count=count,
+                             lang=lang,
+                             max_id=max_id - 1)
     return textdata
 
-    
+
 def output_media(api, query, count, page):
     results = api.search(q=query, count=count)
     media = ""
-    max_id = 0
+    pid = []
     for k in range(0, page):
         for i in range(0, len(results["statuses"])):
-            if "media" in results["statuses"][i]["entities"]:
-                for j in range(0, len(results["statuses"][i]["entities"]["media"])):
-                    media += results["statuses"][i]["entities"]["media"][j]["media_url_https"]
+            d = results["statuses"][i]
+            if "media" in d["entities"]:
+                for j in range(0, len(d["entities"]["media"])):
+                    media += d["entities"]["media"][j]["media_url_https"]
                     media += "\n"
-            max_id = results["statuses"][i]["id"]
-        results = api.search(q=query, count=count, max_id=max_id-1)
-    return media
-    
+            pid.append(d["id"])
+        results = api.search(q=query, count=count, max_id=pid.pop() - 1)
+    return print(media)
+
+
 def output_raw(api, query, count):
-    return pyaml.dump(api.search(q=query, count=count), sys.stdout, vspacing=[0, 1])
+    return pyaml.dump(
+        api.search(q=query,
+                   count=count),
+        sys.stdout,
+        vspacing=[0, 1])
+
 
 def output_simplify(api, query, count, page):
     results = api.search(q=query, count=count)
     tweets = []
+    d = ""
     for j in range(0, page):
         for i in range(0, len(results["statuses"])):
-            who   = results["statuses"][i]["user"]["screen_name"]
-            what  = results["statuses"][i]["text"]
-            where = results["statuses"][i]["geo"]
-            when  = results["statuses"][i]["created_at"]
-            tweets.append({"who":who, "what":what, "where":where, "when":when})
-            max_id = results["statuses"][i]["id"]
-        results = api.search(q=query, count=count, max_id=max_id-1)
-    return pyaml.dump(tweets, sys.stdout, vspacing=[0,1])
+            d = results["statuses"][i]
+            tweets.append({"id": d["id"],
+                           "user": d["user"]["screen_name"],
+                           "text": d["text"],
+                           "geo": d["geo"],
+                           "time": d["created_at"]})
+        results = api.search(q=query, count=count, max_id=d["id"] - 1)
+    return pyaml.dump(tweets, sys.stdout, vspacing=[0, 1])
 
 
 def output_data(api, query, count, page, metatype):
     d = ""
     if metatype == "t":
         textdata = text_on_tweet(api, query, count, page, lang="en")
-        d = output_textdata(textdata)
+        d = textmining.output_textdata(textdata)
     elif metatype == "r":
         d = output_raw(api, query, count)
     elif metatype == "m":
         d = output_media(api, query, count, page)
     elif metatype == "j":
         textdata = text_on_tweet(api, query, count, page, lang="ja")
-        d = output_ja_text(textdata,["名詞", "動詞"])
+        d = textmining.output_ja_text(textdata, ["名詞"])
     elif metatype == "s":
         d = output_simplify(api, query, count, page)
 
     return d
 
-    
+
 def main():
-    
+
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'q:t:c:p:h')
     except getopt.GetoptError as err:
         print(str(err))
         usage()
         sys.exit(2)
-    
+
     query = ""
     metatype = 't'
     count = 1
     page = 1
-    
+
     for o, a in opts:
         if o == "-q":
             query = a
@@ -103,10 +125,12 @@ def main():
             count = int(a)
         elif o == "-p":
             page = int(a)
-    
-    api = load_api(data["consumer"]["key"], data["consumer"]["secret"], data["token"]["key"], data["token"]["secret"])
 
-    print(u"%s" % output_data(api, query, count, page, metatype))
+    api = load_api(data["consumer"]["key"], data["consumer"]["secret"],
+                   data["token"]["key"], data["token"]["secret"])
+
+    output_data(api, query, count, page, metatype)
+
 
 if __name__ == "__main__":
     main()
